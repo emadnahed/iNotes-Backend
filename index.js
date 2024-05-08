0cdd551eb5c4a5b68d6ac60871f314e1b6710e04
emaadnahed <emaadnahed@gmail.com>
2024-05-08 21:13:52 +0530

    webpack mira

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = 5000;
 app.use(express.json())
 
 // Below is the auth route created.
-app.use("/api/auth", require("./routes/auth"));
+app.use("/api/auth", require("./routes/authen"));
 
 app.use("/api/notes", require("./routes/notes"));
 
